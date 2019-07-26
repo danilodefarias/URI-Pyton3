91d877707de20ab295744f33249d0097143f80ce
danilodefarias <53351710+danilodefarias@users.noreply.github.com>
2019-07-26 15:55:54 -0300

    Update 1071.py

--- 1071.py
+++ 1071.py
@@ -2,19 +2,21 @@ maior=int(input())
 menor=int(input())
 total=0
 
+#ordenando os números em uma lista
 lista = [maior,menor]
 lista.sort()
-
 maior = lista[1]
 menor = lista[0]
 
+#condição necessária para números pares
 if menor%2 == 0:
     menor = menor + 1
     for n in range(menor,maior,2):
         total = total + n #ou total += n
-        #use aqio o comando 'print('lista dos números %d somados' %n)' para saber quais números você está somando
+        #use aquio o comando 'print('lista dos números %d somados' %n)' para saber quais números você está somando
     print(total)
 
+#condição necessária para números ímpares
 elif menor %2 != 0:
     menor = menor + 1
     for n in range(menor + 1, maior, 2):
